041de275165e4551e90b8a1df3e5d592fe5026a8
Aata-allah Rchidi <atala.rchidi@gmail.com>
2018-03-08 01:57:56 +0000

    set up the final goal at 100 points

--- app.js
+++ app.js
@@ -92,7 +92,7 @@ function hold(){
         document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
         //Check if player won the game
-        if(scores[activePlayer] >= 20){
+        if(scores[activePlayer] >= 100){
             document.querySelector('#name-' + activePlayer).textContent = 'WINNER !'; 
             document.querySelector('.dice').style.display = 'none';
             document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
