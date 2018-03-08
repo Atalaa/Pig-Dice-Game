b2807c3bf497c1b14042f2b0bf0efc0d81b35f51
Aata-allah Rchidi <atala.rchidi@gmail.com>
2018-03-08 00:13:14 +0000

    added intro alert and cleanup code for deploy

--- Pig-Game files/app.js
+++ Pig-Game files/app.js
@@ -1,33 +1,34 @@
 /*
 GAME RULES:
 
-- The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
-- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
-- The first player to reach 100 points on GLOBAL score wins the game
+- The game has 2 players, playing in rounds.
+
+- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score.
+
+- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn.
+
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLOBAL score. After that, it's the next player's turn.
+
+- The first player to reach 100 points on GLOBAL score wins the game.
+
+ENJOY!
 
 */
 
-//The querySelector() method returns the first element that matches a specified CSS selector(s) in the document
-
-//The textContent property sets or returns the textual content(as it is) of the specified node, and all its descendants.
-//document.querySelector('#current-' + activePlayer).textContent = dice; 
-
-//Return the innerHTML property: HTMLElementObject.innerHTML
-//Set the innerHTML property: HTMLElementObject.innerHTML = text
-//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';     
-
-//document.querySelector('#current-' + activePlayer).textContent = '<em>' + dice + '</em>';     
-//will output plain text: <em>3</em>
-
 var scores, roundScore, activePlayer, gamePlaying;
+
+var intro = ' ***** Welcome to Pig Dice Game *****\n\nThe game has 2 players, playing in rounds.\n\n' + 
+            'In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score.\n\n'
+            + 'BUT, if the player rolls a 1, THE DICE WILL NOT SHOW UP and all his ROUND score gets lost. After that, it\'s the next player\'s turn.\n\n'
+            + 'The player can choose to \'Hold\', which means that his ROUND score gets added to his GLOBAL score. After that, it\'s the next player\'s turn.\n\n'
+            + 'The first player to reach 100 points on GLOBAL score wins the game.\n\n' + 'ENJOY !';
+
+alert(intro);
 startingGame();
 
 //ROLL DICE EVENT
 document.querySelector('.btn-roll').addEventListener('click', rollDice);
                                                                   
-
 //HOLD EVENT
 document.querySelector('.btn-hold').addEventListener('click', hold);
 
@@ -35,13 +36,10 @@ document.querySelector('.btn-hold').addEventListener('click', hold);
 document.querySelector('.btn-new').addEventListener('click', startingGame);
 
 
-
-
-
 //EXTERNAL FUNCTIONS
 function startingGame(){
     scores = [0,0];
-    activePlayer = 0; // 0: player1 begins, 1: player2 plays
+    activePlayer = 0; // if 0 = player1 and if 1 = player2 
     roundScore = 0;
     gamePlaying = true;
     
@@ -64,14 +62,15 @@ function startingGame(){
 
 function rollDice(){     
     if (gamePlaying) {
-        // 1. Random number                                                    
+        
+        // 1. Random number      
         var dice = Math.floor(Math.random() * 6) + 1;
 
         // 2. Display the dice result
         var diceDOM = document.querySelector('.dice');
         diceDOM.style.display = 'block';
-        diceDOM.src = 'dice-' + dice + '.png'
-
+        diceDOM.src = 'dice-' + dice + '.png';
+            
         // 3. Update the round score IF the rolled number was NOT a 1
         if(dice !== 1){
             roundScore += dice;
@@ -80,11 +79,12 @@ function rollDice(){
             nextPlayer();
         }
     }
- }; 
+ };
 
 
 function hold(){
     if (gamePlaying) {
+        
         // 1. Add the current score to the global score
         scores[activePlayer] += roundScore;
 
@@ -103,7 +103,6 @@ function hold(){
         }
     }
 };
-
 
 
 function nextPlayer(){
